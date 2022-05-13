c53a0c8103299f2322a17d1ef24d7949ad002e64
Aleksei Rostov <aleksei.rostov@fortis.online>
2022-05-13 16:24:46 +0300

    Created project

---
+++ Arrays/Problems/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,22 @@
+﻿namespace Arrays.Problems;
+
+public class RemoveDuplicatesFromSortedArray
+{
+    public int RemoveDuplicates(int[] nums) {
+        var hashSet = new HashSet<int>();
+        
+        foreach(var item in nums){
+            if(!hashSet.Contains(item))
+                hashSet.Add(item);
+        }
+        
+        int length = 0;
+        
+        foreach(var item in hashSet){
+            nums[length] = item;
+            length++;
+        }
+        
+        return length;
+    }
+}
---
+++ ArraysTests/RemoveDuplicatesFromSortedArrayTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Arrays.Problems;
+using Xunit;
+using FluentAssertions;
+
+namespace ArraysTests;
+
+public class RemoveDuplicatesFromSortedArrayTests
+{
+    [Fact]
+    public void GetLengthForArrayWithDuplicate()
+    {
+        // создаем тестовые данные
+        var array = new[] {1, 1, 2};
+        var solution = new RemoveDuplicatesFromSortedArray();
+
+        // вызываем код который необходимо проверить
+        var countOfNumber = solution.RemoveDuplicates(array);
+
+        // проверяем что результат соответствует ожидаемому значению
+        // используется библиотека FluentAssertions, подробнее ознакомится можно тут https://fluentassertions.com/introduction
+        countOfNumber.Should().Be(2);
+    }
+    
+    [Fact]
+    public void GetLengthForArrayWithMultipleDuplicate()
+    {
+        var array = new[] {1, 1, 2, 3, 4, 3, 5, 1, 2};
+        var solution = new RemoveDuplicatesFromSortedArray();
+
+        var countOfNumber = solution.RemoveDuplicates(array);
+
+        countOfNumber.Should().Be(5);
+    }
+    
+    [Fact]
+    public void GetZeroForEmptyArray()
+    {
+        var array = Array.Empty<int>();
+        var solution = new RemoveDuplicatesFromSortedArray();
+
+        var countOfNumber = solution.RemoveDuplicates(array);
+
+        countOfNumber.Should().Be(0);
+    }
+    
+    [Fact]
+    public void GetLengthForArrayWithoutDuplicate()
+    {
+        var array = new[] {1, 2, 3, 4, 5};
+        var solution = new RemoveDuplicatesFromSortedArray();
+
+        var countOfNumber = solution.RemoveDuplicates(array);
+
+        countOfNumber.Should().Be(5);
+    }
+}
